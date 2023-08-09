924d4c3bad9b6a8c31b04d3b28c649174e37f257
shinnyeonki <sygys10293@gmail.com>
2023-08-10 06:57:10 +0900

    edit alias

--- auto_install.sh
+++ auto_install.sh
@@ -2,10 +2,11 @@
 # 사용자에게 wsl 환경인지 물어본후 그에 맞게 설치
 echo "WSL 환경인가요? (y/n)"
 read answer
-if [ "$answer" != "${answer#[Yy]}" ] ; # wsl 환경인경우
+#wsl 환경이 아니라면
+if [ "$answer" != "${answer#[Yy]}" ] ;
     then sed -i '/8/d' .shell/alias
 fi
-cp ~/.bashrc .shell/default
+cp ~/.bashrc .shell/defaults
 cp .bashrc ~/.bashrc
 cp -r .shell ~/.shell
 source ~/.bashrc
