71e568a4ff3d9ff544823bffc68910a9038f0880
shinnyeonki <sygys10293@gmail.com>
2023-08-10 07:18:43 +0900

    temp

--- auto_install.sh
+++ auto_install.sh
@@ -1,11 +1,4 @@
 #/bin/sh
-# 사용자에게 wsl 환경인지 물어본후 그에 맞게 설치
-echo "WSL 환경인가요? (y/n)"
-read answer
-#wsl 환경이 아니라면
-if [ "$answer" = "${answer#[Nn]}" ] ; then
-    sed -i '8d' .shell/alias
-fi
 cp ~/.bashrc .shell/defaults
 cp .bashrc ~/.bashrc
 cp -r .shell ~/.shell
